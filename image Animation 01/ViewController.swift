c0475928b7c4ccb0ee5d2c352f3baa79da30ac55
D7703_07 <d7703_07@lulzsec.local>
2019-03-27 14:49:38 +0900

    이미지 반복

--- image Animation 01/ViewController.swift
+++ image Animation 01/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var alienlmageView: UIImageView!
+    
+    var count = 1
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //초기 이미지 보이기
+        alienlmageView.image = UIImage(named: "frame\(count).png")
+        myLabel.text = String(count)
     }
 
-
+    @IBAction func updateButtonPressed(_ sender: Any) {
+        count+=1
+        
+        if count == 6 {
+            count = 1
+        }
+        alienlmageView.image = UIImage(named: "frame\(count).png")
+        
+        myLabel.text = "frame\(count).png"
+        //print("button pressed!")
+        //누를때 마다 count가 1증가
+        //count+=1 //count = count +1
+        //myLabel.text = String(count)
+        //alienlmageView.image = UIImage(named: "frame\(count).png")
+      
+    }
 }
 
